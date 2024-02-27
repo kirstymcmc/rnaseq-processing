9371a1fdc8a851cff067912f9fe0cebb3d7b665d
Kirsty McCready <u2095442@warwick.ac.uk>
2024-02-27 17:43:07 +0000

    updated filepaths

--- quantify_salmon.sh
+++ quantify_salmon.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-#SBATCH --qos castles
+#SBATCH --qos bbdefault
 #SBATCH --account plackarg-spl-bioinformatic-analysis
-#SBATCH --ntasks 8 # request 8 cores for the job. N.B. check whether the fastq-dump can parallelise, else this is redundant and you should set to "1"
+#SBATCH --mem-per-cpu=6750M
+#SBATCH --ntasks 54 # request 54 cores for the job. 
 #SBATCH --nodes 1 # restrict the job to a single node. Necessary if requesting more than --ntasks=1
-#SBATCH --time 1000 # this requests 2 hours, but you will need to adjust depending on runtime. Test job execution time with just a couple of input files then scale accordingly
-#SBATCH --mail-type ALL 
+#SBATCH --time 12:00:00 # this requests 12 hours, 
+#SBATCH --mail-type ALL
 
 set -e
 
 module purge;
 module load bluebear
-module load bear-apps/2021b
-module load Salmon/1.9.0-GCC-11.2.0
+module load bear-apps/2022b
+module load Salmon/1.10.1-GCC-12.2.0
 
+mkdir -p ../data/3_quants
 
-REF_INDEX=ref/salmon_index
+REF_INDEX=../ref/salmon_index
 FILES=../data/2_trimmed/*1_trimmo.fq.gz
-MAPPED_FILES=data/quants
+MAPPED_FILES=data/3_quants
 
 
 for f in $FILES
@@ -30,6 +32,7 @@ f=${f%_1_trimmo.fq.gz}
 salmon quant -i $REF_INDEX -l A \
 -1 ../data/2_trimmed/${f}_1_trimmo.fq.gz \
 -2 ../data/2_trimmed/${f}_2_trimmo.fq.gz \
--p 12 --validateMappings -o $MAPPED_FILES/${f}_quant
+-p 54 --validateMappings -o $MAPPED_FILES/${f} --writeUnmappedNames \
+--gcBias --seqBias
 
 done
