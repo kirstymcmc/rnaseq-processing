00792dded756c44b924e7e7d2ecdc1260f4f6255
Kirsty McCready <u2095442@warwick.ac.uk>
2024-02-28 14:55:52 +0000

    added script submission script

---
+++ submit_extract_unmapped.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --qos bbdefault
+#SBATCH --account plackarg-spl-bioinformatic-analysis
+#SBATCH --ntasks 12 # request 12 cores for the job. N.B. check whether the fastq-dump can parallelise, else this is redundant and you should set to "1"
+#SBATCH --nodes 1 # restrict the job to a single node. Necessary if requesting more than --ntasks=1
+#SBATCH --time 1000 # this requests 2 hours, but you will need to adjust depending on runtime. Test job execution time with just a couple of input files then scale accordingly
+#SBATCH --mail-type ALL 
+
+
+module purge;
+module load bluebear
+
+
+#run from inside data file 
+
+../rnaseq-processing/extract_unmapped.sh . ./2_trimmed ./3_unmapped "u"
