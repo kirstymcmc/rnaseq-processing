ec7270ba90c0a15b84a335306c1b808b8feb3b8e
Kirsty McCready <u2095442@warwick.ac.uk>
2024-03-06 11:10:44 +0000

    added extract_unmapped

--- quantify_salmon.sh
+++ quantify_salmon.sh
@@ -15,24 +15,47 @@ module load bluebear
 module load bear-apps/2022b
 module load Salmon/1.10.1-GCC-12.2.0
 
-mkdir -p ../data/3_quants
-
-REF_INDEX=../ref/salmon_index
-FILES=../data/2_trimmed/*1_trimmo.fq.gz
-MAPPED_FILES=../data/3_quants
 
 
-for f in $FILES
-do
+# Define work folders 
+workdir="../data"
+fqdir="$workdir/2_trimmed"
 
-f=${f##*/}
-f=${f%_1_trimmo.fq.gz}
+refdir=../ref/salmon_index
+outdir="$workdir/3_quants"
 
+echo "Salmon quantification started" >> $workdir/pipeline_log.txt
+echo Current Date and Time is: `date +"%Y-%m-%d %T"` >> $workdir/pipeline_log.txt
 
-salmon quant -i $REF_INDEX -l A \
--1 ../data/2_trimmed/${f}_1_trimmo.fq.gz \
--2 ../data/2_trimmed/${f}_2_trimmo.fq.gz \
--p 54 --validateMappings -o $MAPPED_FILES/${f} --writeUnmappedNames \
+# Check if the output directory exists, if not, create it
+if [ ! -d "$outdir" ]; then
+  mkdir -p "$outdir"
+fi
+
+for r1 in "$fqdir"/*1.fq.gz; do
+    r2=${r1%1.fq.gz}2.fq.gz
+    out_name=$(basename "$r1" | cut -d '_' -f 1-3)
+    if [[ -f $r2 ]] ; then
+
+salmon quant -i $refdir -l A \
+-1 "$r1" -2 "$r2" \
+-p 54 --validateMappings -o $outdir/$out_name --writeUnmappedNames \
 --gcBias --seqBias
-
+    else
+        echo "$r2 not found" >&2
+    fi
 done
+
+
+echo "Salmon quantification completed. Extracting unmapped reads..." >> $workdir/pipeline_log.txt
+echo Current Date and Time is: `date +"%Y-%m-%d %T"` >> $workdir/pipeline_log.txt
+# Unmapped names are written to 3_quants/${f}/aux_info/unmapped_names.txt
+# extract names and seqs using extract_unmapped.sh
+# takes as input (in order): 1. data folder (directory containing 1_raw, 2_trimmed etc)
+#                            2. fastq folder containing all reads (eg 2_trimmed)
+#                            3. output folder name (created within the script
+#                            4. flag (one of "u", "d", "m1", "m2") 
+
+./extract_unmapped.sh $workdir $fqdir $outdir "u"
+
+echo "Unmapped reads extracted" >> $workdir/pipeline_log.txt
